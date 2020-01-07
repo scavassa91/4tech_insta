e66b4e86e8f5269e7aa72cf153e56dca18fbc400
scavassa91 <dan.scavassa@gmail.com>
2020-01-07 19:41:45 -0300

    Added TimeLine container

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 
 import Login from './containers/Login/Login';
+import TimeLine from './containers/TimeLine/TimeLine';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route exact path="/timeline" component={TimeLine} />
         <Redirect to="/" />
       </Switch>
     </Router>
---
+++ src/containers/TimeLine/TimeLine.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TimeLine = () => {
+    return (
+        <div>Time line works!</div>
+    );
+};
+
+export default TimeLine;
