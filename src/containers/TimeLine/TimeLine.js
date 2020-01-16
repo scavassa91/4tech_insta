47007434f50d3956f0f87bee1e2376bd65f5eaeb
Danilo Bevilacqua Scavassa <vntdasc@suportes-MacBook-Pro-2.local>
2020-01-16 00:57:59 -0300

    Implemented upload/post image

--- src/containers/TimeLine/TimeLine.js
+++ src/containers/TimeLine/TimeLine.js
@@ -1,19 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from '../../components/Header/Header';
-import { Container } from '@material-ui/core';
+import { Container, Grid, Paper, InputBase, Button } from '@material-ui/core';
+import { DropzoneArea } from 'material-ui-dropzone'
 
 import Card from '../../components/Card/Card';
 
 import './TimeLine.css';
 
-const TimeLine = () => {
-    
 
+const TimeLine = () => {
+    const [image, setImage] = useState([]);
+
+    const handleChange = (files) => {
+        setImage(files);
+    }
     return (
         <Fragment>
             <Header />
             <Container className="timeline">
+                <Grid item xs={12} className="grid postcard">
+                    <Paper className="paper">
+                        <form className="form">
+                            <InputBase
+                                fullWidth
+                                placeholder="Add a comment..." />
+                            <DropzoneArea
+                                dropzoneText={image.length === 0 ? 'Select or drop your image' : ''}
+                                dropzoneClass="droparea"
+                                onChange={handleChange}
+                                filesLimit={1}
+                                acceptedFiles={	['image/*']}
+                                showAlerts={false}
+                            />
+                            <Button className="postbutton" type="submit" color="primary" disabled={image.length === 0}>Post</Button>
+                        </form>
+                    </Paper>
+                </Grid>
                 <Card />
             </Container>
         </Fragment>
